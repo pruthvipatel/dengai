db4c67e427b28f8433f6aa14f0f2d97551fac15b
Pruthvi Patel <pruthvipatel@users.noreply.github.com>
2023-05-29 22:12:57 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To predict Dengue pandemix',
+    author='Pruthvi Patel',
+    license='MIT',
+)
